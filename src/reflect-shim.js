8fd498226401288c15d6c36610b39b1183113567
mohwa <yanione2@gmail.com>
2018-04-16 00:49:36 +0900

    Reflect 1차 구현 완료
    TODO:
    - 소스 최종 정리
    - 코드 주석
    - 문서화

--- src/reflect-shim.js
+++ src/reflect-shim.js
@@ -3,6 +3,7 @@
  */
 
 const root = window || this;
+const msg = 'Reflect.{{methodName}} called on non-object';
 
 module.exports = !root.Reflect || function(){
 
@@ -11,33 +12,54 @@ module.exports = !root.Reflect || function(){
         construct: _construct,
         defineProperty: _defineProperty,
         deleteProperty: _deleteProperty,
-        get: _get
+        get: _get,
+        set: _set,
+        getOwnPropertyDescriptor: _getOwnPropertyDescriptor,
+        getPrototypeOf: _getPrototypeOf,
+        has: _has,
+        isExtensible: _isExtensible,
+        preventExtensions: _preventExtensions,
+        ownKeys: _ownKeys,
+        setPrototypeOf: _setPrototypeOf
     }
 }();
 
 /**
+ * 전달받은 아규먼트로(target, thisArgument ..) target 함수를 호출한다.
  *
  * @param fn
- * @param context
+ * @param thisArgument
  * @param args
  * @private
  */
-function _apply(fn = function(){}, context = null, args = []){
-    return fn.apply(context, args);
+function _apply(fn = function(){}, thisArgument = null, args = []){
+
+    if (typeof fn !== 'function'){
+        throw new Error(_replaceErrorMsg('apply'));
+    }
+
+    return fn.apply(thisArgument, args);
 }
 
 /**
+ * 전달받은 아규먼트로(target, thisArgument ..) 새로운 인스턴스를 반환한다.
  *
  * @param fn
  * @param args
- * @returns {fn.bind.apply}
+ * @returns {function(this:Function)}
  * @private
  */
 function _construct(fn = function(){}, args = []){
+
+    if (typeof fn !== 'function'){
+        throw new Error(_replaceErrorMsg('construct'));
+    }
+
     return new (fn.bind.apply(fn, [fn].concat(args)));
 }
 
 /**
+ * 객체 속성을 정의한다.
  *
  * @param o
  * @param k
@@ -46,28 +68,248 @@ function _construct(fn = function(){}, args = []){
  * @private
  */
 function _defineProperty(o = null, k = '', disc = null){
-    return Object.defineProperty(o, k, disc);
+
+    if (typeof o !== 'object'){
+        throw new Error(_replaceErrorMsg('defineProperty'));
+    }
+
+    let ret = false;
+
+    try{
+        Object.defineProperty(o, k, disc);
+        ret = true;
+    }
+    catch(e){
+        console.warn(e.message);
+    }
+
+    return ret;
 }
 
 /**
+ * 객체 속성을 삭제한다.
  *
  * @param o
  * @param k
  * @private
  */
 function _deleteProperty(o = null, k = ''){
-    delete o[k];
+
+    if (typeof o !== 'object'){
+        throw new Error(_replaceErrorMsg('deleteProperty'));
+    }
+
+    let ret = false;
+
+    try{
+        delete o[k];
+        ret = true;
+    }
+    catch(e){
+        console.warn(e.message);
+    }
+
+    return ret;
 }
 
 /**
- * 구현해야하는 메서드
  *
  * @param o
  * @param k
+ * @param receiver
+ * @returns {*}
  * @private
  */
-function _get(o = null, k = ''){
-    //delete o[k];
+function _get(o = null, k = '', receiver = null){
+
+    if (typeof o !== 'object'){
+        throw new Error(_replaceErrorMsg('get'));
+    }
+
+    const disc = _getOwnPropertyDescriptor(o, k);
+
+    if (
+        disc &&
+        typeof disc.get === 'function'){
+
+        _defineProperty(o, k, {
+            get: disc.get.bind(receiver || o)
+        });
+    }
+
+    return o[k];
+}
+
+/**
+ *
+ * @param o
+ * @param k
+ * @param v
+ * @param receiver
+ * @returns {boolean}
+ * @private
+ */
+function _set(o = null, k = '', v, receiver = null){
+
+    if (typeof o !== 'object'){
+        throw new Error(_replaceErrorMsg('set'));
+    }
+
+    let ret = false;
+    const disc = _getOwnPropertyDescriptor(o, k);
+
+    if (
+        disc &&
+        typeof disc.set === 'function'){
+
+        _defineProperty(o, k, {
+            set: disc.set.bind(receiver || o)
+        });
+    }
+
+    try{
+        o[k] = v;
+        ret = true;
+    }
+    catch(e){
+        console.warn(e.message);
+    }
+
+    return ret;
+}
+
+/**
+ *
+ * @param o
+ * @param v
+ * @returns {Object}
+ * @private
+ */
+function _getOwnPropertyDescriptor(o = null, v = null){
+
+    if (typeof o !== 'object'){
+        throw new Error(_replaceErrorMsg('getOwnPropertyDescriptor'));
+    }
+
+    return Object.getOwnPropertyDescriptor(o, v);
+}
+
+
+/**
+ *
+ * @param o
+ * @returns {Object}
+ * @private
+ */
+function _getPrototypeOf(o = null){
+
+    if (typeof o !== 'object'){
+        throw new Error(_replaceErrorMsg('getPrototypeOf'));
+    }
+
+    return Object.getPrototypeOf(o);
+}
+
+/**
+ *
+ * @param o
+ * @param k
+ * @returns {boolean}
+ * @private
+ */
+function _has(o = null, k = ''){
+
+    if (typeof o !== 'object'){
+        throw new Error(_replaceErrorMsg('has'));
+    }
+
+    return k in o;
+}
+
+/**
+ *
+ * @param o
+ * @returns {boolean}
+ * @private
+ */
+function _isExtensible(o = null){
+
+    if (typeof o !== 'object'){
+        throw new Error(_replaceErrorMsg('isExtensible'));
+    }
+
+    return Object.isExtensible(o);
+}
+
+/**
+ *
+ * @param o
+ * @returns {boolean}
+ * @private
+ */
+function _preventExtensions(o = null){
+
+    if (typeof o !== 'object'){
+        throw new Error(_replaceErrorMsg('preventExtensions'));
+    }
+
+    return Object.preventExtensions(o);
+}
+
+/**
+ *
+ * @param o
+ * @returns {Object}
+ * @private
+ */
+function _ownKeys(o = null){
+
+    if (typeof o !== 'object'){
+        throw new Error(_replaceErrorMsg('ownKeys'));
+    }
+
+    return Object.keys(o);
+}
+
+/**
+ *
+ * @param o
+ * @returns {Array}
+ * @private
+ */
+function _setPrototypeOf(o = null, prototype = null){
+
+    if (typeof o !== 'object'){
+        throw new Error(_replaceErrorMsg('setPrototypeOf'));
+    }
+
+    if (typeof prototype !== 'object'){
+        throw new Error(_replaceErrorMsg('setPrototypeOf'));
+    }
+
+    let ret = false;
+
+    try{
+
+        Object.setPrototypeOf(o, prototype);
+
+        ret = true;
+    }
+    catch(e){
+        console.warn(e.message);
+    }
+
+    return ret;
+}
+
+/**
+ *
+ * @param methodName
+ * @returns {*}
+ * @private
+ */
+function _replaceErrorMsg(methodName = ''){
+    return msg.replace(/\{\{(methodName)\}\}/, methodName);
 }
 
 
--- test/index.js
+++ test/index.js
@@ -5,30 +5,60 @@
 
 const Reflect = require('reflect-shim');
 
-function func1(a, b, c) {
-  this.sum = a + b + c;
-}
+//const object1 = {};
+//
+//console.log(Reflect.defineProperty(object1, 'property1', {value: 42}));
+//
+//if (Reflect.defineProperty(object1, 'property1', {value: 42})) {
+//  console.log('property1 created!');
+//  // expected output: "property1 created!"
+//} else {
+//  console.log('problem creating property1');
+//}
+//
+//console.log(object1.property1);
+//// expected output: 42
+//
+//var obj = { x: 1, y: 2 };
+//console.log(Reflect.deleteProperty(obj, 'x')); // true
+//obj; // { y: 2 }
+//
+//var arr = [1, 2, 3, 4, 5];
+//Reflect.deleteProperty(arr, '3'); // true
+//arr; // [1, 2, 3, , 5]
+//
+//// Returns true if no such property exists
+//Reflect.deleteProperty({}, 'foo'); // true
+//
+//// Returns false if a property is unconfigurable
+//console.log(Reflect.deleteProperty(Object.freeze({foo: 1}), 'foo')); // false
 
-const args = [1, 2, 3];
-const object1 = new func1(...args);
+var obj = {
+  get foo() { console.log(this); return this.bar(); },
+  set foo (v) { console.log(this); },
+  bar: function() {  }
+};
 
-const object2 = Reflect.construct(func1, args);
+//Reflect.get(obj, "foo", {x: 2});
+Reflect.set(obj, "foo", "vvvvv", {x: 2});
+//
+//// Object
+//var obj = { x: 1, y: 2 };
+//console.log(Reflect.get(obj, 'x')); // 1
+//
+//// Array
+//console.log(Reflect.get(['zero', 'one'], 1)); // "one"
 
-console.log(object2.sum);
-// expected output: 6
+// Proxy with a get handler
+//var x = {p: 1};
+//var obj = new Proxy(x, {
+//  get(t, k, r) { console.log(k); return k + 'bar'; }
+//});
+//
+//var a = Reflect.get(obj, 'foo'); // "foobar"
+//
+//console.log(a);
 
-console.log(object1.sum);
-// expected output: 6
-
-
-console.log(Reflect.apply(Math.floor, undefined, [1.75]));
-// expected output: 1
-
-console.log(Reflect.apply(String.fromCharCode, undefined, [104, 101, 108, 108, 111]));
-// expected output: "hello"
-
-console.log(Reflect.apply(RegExp.prototype.exec, /ab/, ['confabulation']).index);
-// expected output: 4
-
-console.log(Reflect.apply(''.charAt, 'ponies', [3]));
-// expected output: "i"
+//console.log(obj['foo']);
+ // if obj[name] is an accessor, it gets run with `this === wrapper`
+//console.log(Reflect.set(obj, 'foo', 3, {x: 3}));
